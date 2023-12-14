55b7d98f504b174f568ea54cf2aa72e337276a8f
iamgymie <6257063+gymie@users.noreply.github.com>
2023-12-14 12:03:11 +0700

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  View,
+  Text,
+  Button,
+  TextInput,
+  Image,
+  FlatList,
+  StyleSheet,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.tulisan}>Hello nama saya budi</Text>
+      <Text style={styles.tulisan}>Saya sedang belajar React Native</Text>
+      <Text style={styles.tulisan}>Membuat Aplikasi Mobile</Text>
+      <Button title="Click Saya" onPress={() => alert("Hello")} />
+      <Button title="Keluar" onPress={() => alert("Keluar")} color="red" />
+      <TextInput placeholder="Input nama" style={{ color: "white" }} />
+      <Image
+        source={{
+          uri: "https://picsum.photos/200/300",
+        }}
+        style={{ width: 200, height: 300 }}
+      />
+      <FlatList
+        data={[
+          {
+            id: 1,
+            nama: "Apel",
+          },
+          {
+            id: 2,
+            nama: "Jeruk",
+          },
+          {
+            id: 3,
+            nama: "Mangga",
+          },
+          {
+            id: 4,
+            nama: "Semangka",
+          },
+        ]}
+        renderItem={({ item }) => (
+          <Text style={styles.tulisan}>{item.nama}</Text>
+        )}
+        keyExtractor={(item) => item.id.toString()}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 30,
+    backgroundColor: "orange",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  tulisan: {
+    color: "red",
+    fontSize: 25,
   },
 });
